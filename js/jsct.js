be79fb217fd7f1ac37c676e28bb08e419c83b9bc
Admin <nghiemthehung1709@gmail.com>
2018-04-16 10:33:46 +0700

    update

--- js/jsct.js
+++ js/jsct.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
 
+    $('#sl-type-order').change(function () {
+
+        nameform=$('#sl-type-order').val();
+        if (nameform=='form-instant'){
+            $('#form-instant').css('display','block');
+            $('#form-pending').css('display','none');
+        }else if(nameform=='form-pending'){
+            $('#form-pending').css('display','block');
+            $('#form-instant').css('display','none');
+        }
+
+    });
+
+
 });
