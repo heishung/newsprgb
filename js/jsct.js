530ff493305f798bf4f7add5c3925e327700dc0b
Admin <nghiemthehung1709@gmail.com>
2018-04-16 16:21:36 +0700

    update

--- js/jsct.js
+++ js/jsct.js
@@ -13,5 +13,26 @@ $(document).ready(function(){
 
     });
 
+    $('#btn-click-whow').click(function () {
+       $('#show-menu').addClass('show');
+    });
+    $('.lang-close-button').click(function () {
+       $('#show-menu').removeClass('show');
+    });
 
+    $('.register-real-account').click(function () {
+
+       $('#box-login-form').css('display','none')
+        $('#register-form').css('display','block')
+        $('body').addClass('body-img-1')
+        $('body').removeClass('body-img-2')
+    });
+    $('.login-real-account').click(function () {
+        $('#box-login-form').css('display','block')
+        $('#register-form').css('display','none')
+        $('body').addClass('body-img-2')
+        $('body').removeClass('body-img-1')
+
+
+    });
 });
